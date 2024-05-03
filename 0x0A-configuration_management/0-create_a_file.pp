e523dce248cb370bf50e619e4624d798f324f4ee
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-05-04 01:57:19 +0300

    Fix puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 $file_path = '/tmp/school'
 
 file { $file_path:
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
-    content => 'I love Puppet'
+    ensure  => file,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
 }
