c3531751b7678d22da8f81b88401cbb51154b9cb
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-05-04 02:21:48 +0300

    Fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# Using Puppet, install flask from pip3.
-
+# Using Puppet, install flask from pip3
 package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-
