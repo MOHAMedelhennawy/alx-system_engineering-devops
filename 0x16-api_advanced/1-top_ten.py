33c7d2960454e515cbcde585e15c42fa99276ca1
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-06-19 03:49:42 +0300

    Add Task-1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""Contains top_ten function"""
+import requests
 
 
 def top_ten(subreddit):
-    """
-    prints the titles of the first 10 hot posts listed for a given subreddit.
-    """
-    import requests
-    headers = {'User-Agent': "My-User-Agent"}
-
-    res = requests.get('https://oauth.reddit.com/r/{}/hot.json?limit=10 '
-                       .format(subreddit),
-                       headers=headers)
-
-    if res.status_code != 200:
-        print('None')
-    else:
-        for post in res.json()['data']['children']:
-            print(post['data']['title'])
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
